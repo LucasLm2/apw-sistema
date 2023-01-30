1695d214c2683a56bce10d11be865698fad487ce
LucasLm2 <lucas_pss13@hotmail.com>
2023-01-30 10:18:43 -0300

    Criação de Migration, Controller, Model, FormRequest e view da Reguladora

---
+++ database/migrations/2023_01_22_000002_create_reguladoras_table copy.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reguladoras', function (Blueprint $table) {
+            $table->id();
+            $table->string('cpnj', 14);
+            $table->string('nome');
+            $table->string('inscricao_estadual')->nullable();
+            $table->string('site')->nullable();
+            $table->unsignedBigInteger('endereco_id')->nullable();
+            $table->timestamps();
+
+            // Foreign keys
+            $table->foreign('endereco_id')->references('id')->on('enderecos');
+
+            // Indexs
+            $table->unique('cpnj');
+            $table->index('endereco_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reguladoras');
+    }
+};
