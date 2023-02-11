28a9f30258e7c55906cff00fe0c32894c4431c17
LucasLm2 <lucas_pss13@hotmail.com>
2023-02-10 21:22:24 -0300

    Criando classe de regra para validar se atributo existe na pesquisa em banco

---
+++ app/Rules/Existe.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Rules;
+
+use App\Helpers\ManipulacaoString;
+use Illuminate\Contracts\Validation\InvokableRule;
+use Illuminate\Database\Eloquent\Model;
+
+class Existe implements InvokableRule
+{
+    function __construct(private Model $class)
+    {}
+
+    /**
+     * Run the validation rule.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     * @return void
+     */
+    public function __invoke($attribute, $value, $fail)
+    {
+        if($attribute == 'cnpj') {
+            $value = ManipulacaoString::limpaString($value);
+        }
+
+        if($this->class::where($attribute, '=', $value)->exists()) {
+            $fail('Este :attribute já esta cadastrado.');
+        }
+    }
+}
