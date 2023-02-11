7b39540bd4cf47979a86fea473ba225a493367ef
LucasLm2 <lucas_pss13@hotmail.com>
2023-02-11 14:29:47 -0300

    Configurando Api

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        if($response->status() != 200) {
+            $response->headers->set ('Access-Control-Allow-Origin', "*");
+        }
+
+        return $response;
+    }
+}
