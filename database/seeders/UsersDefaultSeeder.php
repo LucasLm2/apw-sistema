5db68017f87ff59980048568a05f75b4b99ef1f3
LucasLm2 <lucas_pss13@hotmail.com>
2023-02-17 11:22:43 -0300

    Criação de seed para usuários default

---
+++ database/seeders/UsersDefaultSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersDefaultSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Lucas Passos Silva dos Santos',
+            'email' => 'lucas.p@bcode.com.br',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$rxOZ9Qvu4ab9uPwcnireBOewMp4yPRSKg4oVmFFl1PVNP1qpcnDSO',
+            'remember_token' => 'bhgAe2hvFrM4J5WxSBDvEELAZpPKUgiEYcd6PJHLgHjHUVWoxP9Zobs4TCNQ',
+        ]);
+
+        User::create([
+            'name' => 'Claudio Tomaz',
+            'email' => 'tomaz.claudio@bol.com.br',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$v63Nn8wY8rRgo/nzCL48b.juAy14Cs3PUcrDjtM7Lj82gHdb9GifW',
+            'remember_token' => 'KFMEJNp4kwZ2LBASXUHU4uvK9Q3XrCpZD83g9q0OZKOukHNxFoYe3HhKI6hY',
+        ]);
+    }
+}
